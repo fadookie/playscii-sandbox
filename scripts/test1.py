75cc8af3d2dde1d10c75c256ac451d35b39f20fe
Eliot Lash <fadookie@gmail.com>
2018-06-20 13:31:11 -0700

    functional 3d perlin renderer

--- scripts/test1.py
+++ scripts/test1.py
@@ -1,18 +1,32 @@
 from game_object import GameObject
 from art import TileIter
 
+from .opensimplex import OpenSimplex
+
 
 class Canvas(GameObject):
     generate_art = True
     art_width, art_height = 54, 30  # approximately 16x9 aspect
+    simplex = OpenSimplex()
 
     def pre_first_update(self):
         self.art.set_palette_by_name('gameboy')
 
+        # self.set_timer_function('noise', self.noise_permutation, 0.2)
+
     def update(self):
-        for frame, layer, x, y in TileIter(self.art):
-            char = self.art.charset.last_index - 5
-            fg = self.art.palette.get_random_color_index()
-            bg = 3 % len(self.art.palette.colors)
-            self.art.set_tile_at(frame, layer, x, y, char, fg, bg)
+        self.noise_permutation()
         GameObject.update(self)
+
+    def noise_permutation(self):
+        time = self.world.get_elapsed_time() * 0.001
+        xyscale = 0.20
+        for frame, layer, x, y in TileIter(self.art):
+            noise = self.simplex.noise3d(
+                x=x * xyscale,
+                y=y * xyscale,
+                z=time)
+            char = self.art.charset.last_index - 5
+            fg = int((noise + 0.5) * len(self.art.palette.colors))
+            bg = 0 % len(self.art.palette.colors)
+            self.art.set_tile_at(frame, layer, x, y, char, fg, bg)
